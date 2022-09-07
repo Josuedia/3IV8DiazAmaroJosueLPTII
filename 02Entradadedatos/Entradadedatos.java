efdb6d3a960a0639a04e5dd03c4e90426b6ffac7
Josuedia <jdiaza2103@alumno.ipn.mx>
2022-09-07 17:02:34 -0500

    Entradadedatos

---
+++ 02Entradadedatos/Entradadedatos.java
@@ -0,0 +1,25 @@
+//esto es un comentario
+
+ /**
+  *Todo programa en java debe de de tener el nombre de la clase tal cual como elo archivo
+    */
+class Entradadedatos{
+    //los limites de la clase
+
+    //Toda clase debe de tener un metodo principal
+    /**vamos a realizar un programa mediante el cual podemos introducir texto nuestra conveniencia
+     */
+    //el ; es el finde linea
+public static void main (String[] args) {
+//tipo de dato
+ String nombre;
+    // instancia
+    System.out.println("Por favor introduce tu nombre y numero de tarjeta de credito y debito");
+    //sysrem: la clase, out:el objeto, print:el metodo
+    
+    //vamos a obtener el nombre del usuario
+    nombre = System.console().readLine();
+    System.out.println("Hola " + nombre +" bienvenido jaja");
+    } 
+}
+
