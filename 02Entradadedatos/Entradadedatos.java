b344057bad7f401142fee51b8d35a8a2d53ab5c4
Josuedia <jdiaza2103@alumno.ipn.mx>
2022-09-07 17:53:24 -0500

    actualización del codigo, leectura

--- 02Entradadedatos/Entradadedatos.java
+++ 02Entradadedatos/Entradadedatos.java
@@ -8,18 +8,20 @@ class Entradadedatos{
 
     //Toda clase debe de tener un metodo principal
     /**vamos a realizar un programa mediante el cual podemos introducir texto nuestra conveniencia
-     */
+    */
     //el ; es el finde linea
 public static void main (String[] args) {
 //tipo de dato
  String nombre;
+ int edad;
     // instancia
-    System.out.println("Por favor introduce tu nombre y numero de tarjeta de credito y debito");
-    //sysrem: la clase, out:el objeto, print:el metodo
-    
+    System.out.println("Por favor introduce tu nombre, numero de tarjeta de credito y debito: ");
+    //sysrem: la clase, out:el objeto, println:el metodo
     //vamos a obtener el nombre del usuario
     nombre = System.console().readLine();
-    System.out.println("Hola " + nombre +" bienvenido jaja");
+    System.out.println("Introduce tu edad (no se aceptan numeros negativo): ");
+    edad = Integer.parseInt(System.console().readLine());
+    System.out.println("Hola " + nombre +" bienvenido jaja, si salio bien tienes "+ edad + "de edad");
     } 
 }
 
